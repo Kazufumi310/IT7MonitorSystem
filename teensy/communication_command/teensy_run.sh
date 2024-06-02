8f6f2463aa4c2b95dbd7bfd153094c5b26f0d228
kazufumi310 <kazufumi@isee.nagoya-u.ac.jp>
2024-06-02 15:59:23 +0900

    quit removing request files in teensy_run.sh.

--- teensy/communication_command/teensy_run.sh
+++ teensy/communication_command/teensy_run.sh
@@ -12,6 +12,8 @@ if [ $mode -eq 0 ];then
 elif [ $mode -eq 1 ];then
     reqFile=test.request
 fi
-touch $reqFile
+if [ ! -e $reqFile ];then
+    touch $reqFile
+fi
 lftp -u teensy4x,ftp_test -p 21 192.168.11.${IPlastDigit} -e "cd data;put $reqFile;exit"
-rm $reqFile
+
