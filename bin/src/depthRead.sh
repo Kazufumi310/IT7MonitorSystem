5f3af1b9a846a9892d73b2d0c35bf89b353232a8
kazufumi310 <kazufumi@isee.nagoya-u.ac.jp>
2024-07-20 17:31:31 +0900

    modify depthRead.sh

--- bin/src/depthRead.sh
+++ bin/src/depthRead.sh
@@ -3,8 +3,9 @@ while [ 1 ];
 do
     wget http://192.168.11.99 >& /dev/null
     data=`tail -n 1 index.html`
+    data=`echo $data | sed 's:<br>: :g'`
     utime=`date "+%s.%3N"`
-    echo $data" "$utime
+    echo $data" unixtime: "$utime
     rm -f index.html
     sleep 0.5
 done
